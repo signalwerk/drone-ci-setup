af9a6d9f80c76123ae867c80a07cef6bb870add0
signalwerk <sh@signalwerk.ch>
2020-04-22 16:01:21 +0200

    FIX version 1.7 runner for drone

--- start.py
+++ start.py
@@ -2,6 +2,7 @@ import subprocess
 import time
 import urllib2
 import sys
+import httplib
 
 # cmd = "ssh -i ./data/serveo.net/id_rsa -R ci.signalwerk.ch:80:localhost:80 serveo.net"
 cmd = "ssh -p 2222 -R ci:80:localhost:80 ssh-dns.signalwerk.ch"
@@ -10,7 +11,14 @@ cmd = "ssh -p 2222 -R ci:80:localhost:80 ssh-dns.signalwerk.ch"
 def check_url( url, timeout=15 ):
     try:
         return urllib2.urlopen(url,timeout=timeout).getcode() == 200
-    except urllib2.URLError as e:
+    except urllib2.HTTPError, e:
+        print('HTTPError = ' + str(e.code))
+        return False
+    except urllib2.URLError, e:
+        print('URLError = ' + str(e.reason))
+        return False
+    except httplib.HTTPException, e:
+        print('HTTPException')
         return False
 
 
